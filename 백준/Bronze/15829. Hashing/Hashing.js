043a9738ab04f9d7be5ecea91971e992167c61d7
0wkim <94829216+0wkim@users.noreply.github.com>
2026-01-08 17:47:30 +0900

    [Bronze II] Title: Hashing, Time: 96 ms, Memory: 9336 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.js
@@ -0,0 +1,19 @@
+// Hashing
+
+const FS = require('fs').readFileSync(process.platform === "linux"?"/dev/stdin":"./15829.txt");
+const INPUT = FS.toString().split("\n");
+
+const length = Number(INPUT[0]);
+const alphabetString = INPUT[1];
+
+const r = 31;
+const m = 1234567891;
+const alphabetList = Array.from({length: 26}, (_, i) => String.fromCharCode(97+i));
+
+let hashSum = 0;
+for (let i = 0; i < length; i++) {
+    let a = alphabetList.indexOf(alphabetString[i]) + 1;
+    hashSum += a * r**i;
+}
+
+console.log(hashSum);
